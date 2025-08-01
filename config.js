055c6d0c02c1c2f899c98a35778e22bc5e304600
Jayden-official <cutesoul0731@gmail.com>
2025-08-01 09:08:25 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,41 +1,30 @@
 require('dotenv').config();
 
-function mapPresence(val) {
-    const mapping = {
-        typing: 'composing',
-        online: 'available',
-        recording: 'recording',
-        paused: 'paused',
-        offline: 'unavailable'
-    };
-    return mapping[val?.toLowerCase()?.trim()] || 'paused';
-}
+global.APIs = {
+    xteam: 'https://api.xteam.xyz',
+    dzx: 'https://api.dhamzxploit.my.id',
+    lol: 'https://api.lolhuman.xyz',
+    violetics: 'https://violetics.pw',
+    neoxr: 'https://api.neoxr.my.id',
+    zenzapis: 'https://zenzapis.xyz',
+    akuari: 'https://api.akuari.my.id',
+    akuari2: 'https://apimu.my.id',
+    nrtm: 'https://fg-nrtm.ddns.net',
+    bg: 'http://bochil.ddns.net',
+    fgmods: 'https://api-fgmods.ddns.net'
+};
+
+global.APIKeys = {
+    'https://api.xteam.xyz': 'd90a9e986e18778b',
+    'https://api.lolhuman.xyz': '85faf717d0545d14074659ad',
+    'https://api.neoxr.my.id': 'yourkey',
+    'https://violetics.pw': 'beta',
+    'https://zenzapis.xyz': 'yourkey',
+    'https://api-fgmods.ddns.net': 'fg-dylux'
+};
 
 module.exports = {
-    prefixes: process.env.PREFIX
-        ? process.env.PREFIX.split(',').map(p => p.trim())
-        : [''],
-
-    NUMBER: process.env.YOUR_NUMBER || '254794597254',
-    MODE: (process.env.MODE || 'private').toLowerCase().trim(),
-    WARN_LIMIT: process.env.WARNINGS || '3',
-    ON: process.env.YOUR_NAME || 'J-MD',
-    ANTICALL: process.env.ANTICALL || 'on',
-    ADM: process.env.ANTIDELETE || 'on',
-    AR: process.env.AUTO_REACTION || 'off',
-
-    AUTO_VIEW_STATUS: process.env.AUTO_READ_STATUS === 'on',
-    AUTO_LIKE: process.env.AUTO_LIKE === 'on',
-    AUTO_READ_MESSAGES: process.env.AUTO_READ_DM === 'on',
-    HEROKU_API_KEY: process.env.HEROKU_API_KEY,
-    HEROKU_APP_NAME: process.env.HEROKU_APP_NAME,
-    sessionBase64: process.env.SESSION || '',
-    timezone: 'Africa/Nairobi',
-
-    USER_LID: process.env.YOUR_LID, 
-
-    PRESENCE_DM: mapPresence(process.env.PRESENCE_DM || 'typing'),
-    PRESENCE_GROUP: mapPresence(process.env.PRESENCE_GROUP || 'recording'),
-
-    mapPresence
+    WARN_COUNT: 3,
+    APIs: global.APIs,
+    APIKeys: global.APIKeys
 };
