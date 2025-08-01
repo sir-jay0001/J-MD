963feb90822f3c806ee1067585e4fa2a581987df
Jayden-official <cutesoul0731@gmail.com>
2025-08-01 09:04:23 +0300

    Update and rename j.js to settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '𝐉 𝐌ᴅ',
+  author: 'sir-jay0001',
+  botName: "𝐉 𝐌ᴅ",
+  botOwner: 'Jay', // Your name
+  ownerNumber: '254794597254', //Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
